3631ec348dcf3e07a6305800d5bf1e125c97e902
Matthew Ray Lee <matthewraylee@gmail.com>
2023-05-28 23:39:51 -0400

    Updates

---
+++ precache-manifest.1bbda90f81b883915515a72818c85c3b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "33d07d79606ca1df1604",
+    "url": "/static/css/main.ba8a2f12.chunk.css"
+  },
+  {
+    "revision": "33d07d79606ca1df1604",
+    "url": "/static/js/main.0dc746d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a66fa1d801df643c42df",
+    "url": "/static/css/2.34fa76fd.chunk.css"
+  },
+  {
+    "revision": "a66fa1d801df643c42df",
+    "url": "/static/js/2.893334ee.chunk.js"
+  },
+  {
+    "revision": "f80169225dfb4313a0a39ef107f55d50",
+    "url": "/static/media/swiftlogo.f8016922.png"
+  },
+  {
+    "revision": "d3c746b2baa87534de255e2da8d49620",
+    "url": "/static/media/pythonLogo.d3c746b2.png"
+  },
+  {
+    "revision": "65245af12a3ec363b9ebb54fbd621c47",
+    "url": "/static/media/postman.65245af1.svg"
+  },
+  {
+    "revision": "fc6811ffc4730d20e0ab813162bb8608",
+    "url": "/static/media/CARAOKE.fc6811ff.png"
+  },
+  {
+    "revision": "8761864d7b31f1a8404b811c00d9523e",
+    "url": "/static/media/opentoklogo.8761864d.png"
+  },
+  {
+    "revision": "b04e9093798af9c2b7fb49a9c312850c",
+    "url": "/static/media/MNISTpic.b04e9093.jpeg"
+  },
+  {
+    "revision": "3602dbeac7b595e78e75ceda8f0e9002",
+    "url": "/static/media/firebaselogo.3602dbea.png"
+  },
+  {
+    "revision": "bcf7b3c74d00d6ed1601f4bc7bcc015d",
+    "url": "/static/media/numpylogo.bcf7b3c7.png"
+  },
+  {
+    "revision": "4e9beaa6c1eea560a18dcac9e7dced07",
+    "url": "/static/media/arduinoLogo.4e9beaa6.png"
+  },
+  {
+    "revision": "e4f46516b8d0d4b45f958b7e404f2d55",
+    "url": "/static/media/cppLogo.e4f46516.png"
+  },
+  {
+    "revision": "682b59a561d6f9a9243b981e3395c95a",
+    "url": "/static/media/pytorchlogo.682b59a5.png"
+  },
+  {
+    "revision": "1667404823184616f052bb9777636431",
+    "url": "/static/media/MATTLOGO1.16674048.png"
+  },
+  {
+    "revision": "f34a4eeeaf2346ed48637359fa3bd5f0",
+    "url": "/static/media/video.f34a4eee.mp4"
+  },
+  {
+    "revision": "bb04496cbeafb34f0d44dcce8f6e1631",
+    "url": "/static/media/jenkins.bb04496c.png"
+  },
+  {
+    "revision": "d379a1b5e4f49079f1a4a37f76d04c31",
+    "url": "/static/media/mulesoft.d379a1b5.avif"
+  },
+  {
+    "revision": "6c55330a6cd1feb0e3b23321ad008d78",
+    "url": "/static/media/bitbucket.6c55330a.png"
+  },
+  {
+    "revision": "0451ed2e06f1103dd6481951f9ea2a1e",
+    "url": "/static/media/nodejs.0451ed2e.png"
+  },
+  {
+    "revision": "b1b2685a5c313fb0900002e40745cb80",
+    "url": "/static/media/css.b1b2685a.svg"
+  },
+  {
+    "revision": "2169b9f58cedc2229ab27eeb81b454e4",
+    "url": "/static/media/html.2169b9f5.png"
+  },
+  {
+    "revision": "b9d317090d5820e3d705026fe41bccff",
+    "url": "/static/media/mysql.b9d31709.png"
+  },
+  {
+    "revision": "c6321fa44eb75530ac6f0f2ed1e34d3b",
+    "url": "/static/media/matt_about1.c6321fa4.jpg"
+  },
+  {
+    "revision": "9f1d80be6a7b9023736f0c36ea550bf3",
+    "url": "/static/media/golf.9f1d80be.svg"
+  },
+  {
+    "revision": "591251db3fb4a1cea4089a74f1ad10f9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5bbfc678e5e884419bdc7a816aed102.js"
+  "/precache-manifest.1bbda90f81b883915515a72818c85c3b.js"
 );
 
 workbox.clientsClaim();
